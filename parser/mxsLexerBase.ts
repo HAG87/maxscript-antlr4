dcc36ae6c5b8b005662762af65b31be7fdb9e259
Bump-MSI <atelierbump@gmail.com>
2024-07-18 16:43:25 -0600

    mxsLexerBase working state

--- parser/mxsLexerBase.ts
+++ parser/mxsLexerBase.ts
@@ -14,33 +14,19 @@ export abstract class mxsLexerBase extends Lexer
     {
         super(input);
     }
-    // private readonly invalidChars = [9,10,12,13,32];
+
     /*
     private lastToken: Token;
-
-    protected next(char: number): boolean
-    {
-        // this.inputStream.LA(1)
-
-        console.log(this.inputStream.LA(1) === char);
-        // console.log(mxsLexer.NL);
-        console.log(super.nextToken().channel);
-
-        return (this.inputStream.LA(1) === char);
-    }
 
     public override nextToken(): Token
     {
         // Get the next token.
         const next: Token = super.nextToken();
-
-        console.log(`${next.line} : ${next.channel} ${JSON.stringify(next.text)}`);
-
+        // console.log(`${next.line} : ${next.channel} ${JSON.stringify(next.text)}`);
         if (next.channel == Token.DEFAULT_CHANNEL) {
             // Keep track of the last token on the default channel.
             this.lastToken = next;
         }
-
         return next;
     }
     // */
@@ -63,38 +49,28 @@ export abstract class mxsLexerBase extends Lexer
             default:
                 console.log('\x1b[36m%s\x1b[0m', `${this.line} > ${JSON.stringify(this.text)}`);
                 break;
-        }
-        // */
-        // sanitize tokens
-        // if (this.channel === mxsLexer.DEFAULT_TOKEN_CHANNEL && this.type !== mxsLexer.NL)
-        // {
-        //     this.text = this.text.trim();
-        // }
+        } // */
+        //  sanitize tokens
         if (this.type === mxsLexer.NL) this.text = '\r\n';
         return super.emit();
     }
 
-    protected followed(): boolean
+    protected followed(charcode: number = 32): boolean
     {
+        //  console.log(super.nextToken().channel);
         //  console.log(this.text.charCodeAt(0));
         //  console.log(`followed: ${(this.inputStream.LA(1) > 32) && (this.inputStream.LA(1) !== 45)} -> ${JSON.stringify(String.fromCharCode(this.inputStream.LA(1)))}`);
         return (
-            this.inputStream.LA(1) > 32 &&
+            this.inputStream.LA(1) > charcode &&
             this.inputStream.LA(1) !== this.text.charCodeAt(0)
         );
-        //  return (this.inputStream.LA(1) > 32);
-        //  return false;
     }
 
-    protected preceeded(): boolean
+    protected preceeded(charcode: number = 32): boolean
     {
-        //  console.log(this.text.charCodeAt(0));
-        //  console.log(`followed: ${(this.inputStream.LA(1) > 32) && (this.inputStream.LA(1) !== 45)} -> ${JSON.stringify(String.fromCharCode(this.inputStream.LA(1)))}`);
         return (
-            this.inputStream.LA(-1) > 32 &&
+            this.inputStream.LA(-1) > charcode &&
             this.inputStream.LA(-1) !== this.text.charCodeAt(0)
         );
-        //  return (this.inputStream.LA(1) > 32);
-        //  return false;
     }
 }
